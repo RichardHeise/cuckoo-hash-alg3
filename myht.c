5793ce4d3ab9f2d91816ef73a2c670d0943fd887
Richard Heise <rfhf19@inf.ufpr.br>
2021-02-18 22:09:10 -0300

    setting up for exclusion

--- myht.c
+++ myht.c
@@ -2,19 +2,19 @@
 #include<stdio.h>
 #include"myht.h"
 
-int insert(int key, int table_1[], int table_2[]){
+int insert(int key, int table_1[], int table_2[]) {
 
 	int k;
+	k = hash_1(key); /*we hash the key first*/
 
-	k = key % M;
-	if (table_1[k] == key) /*if table[k' already exists*/
+	if (table_1[k] == key) /*if table[k] already exists*/
 		return k;
-	else if (table_1[k] == FLAG) /*if table[k] is free*/
+	else if (table_1[k] == FLAG /*|| table_1[k] == OPEN*/ ) /*if table[k] is free*/
 		table_1[k]=key;
-	else{
-		int j;
+	else {
+		int j; /*variable that'll receive the new pos of key on table 2*/
 		j = table_1[k];
-		j = floor(M * (j * 0.9 - floor(j * 0.9)));
+		j = hash_2(j);
 		table_2[j] = table_1[k];
 		table_1[k] = key;
 	}
@@ -25,7 +25,7 @@ int insert(int key, int table_1[], int table_2[]){
 void printTable(int table[]){
 
 	int i;
-	for(i=0;i<M;i++){
+	for(i = 0; i < M; i++){
 		//if(i[M] != FLAG);
 		printf("%d %d\n",i, table[i]);
 	}
@@ -35,26 +35,32 @@ void printTable(int table[]){
 int search(int key, int table_1[], int table_2[]) {
 
 	int k;
-	k = key % M;
+	k = hash_1(key); /* find the position where key should be on table 1*/
 
-	if (table_1[k] != FLAG) {
+	if (table_1[k] != FLAG /*AND table_1[k] != OPEN*/ ) { /*if position is ocupied*/
 		if (table_1[k] == key) {
 			return k;
 		} else {
-			int j;
-			j = floor(M * (key * 0.9 - floor(key * 0.9)));
-
-			return j;
+			return hash_2(key); /*return pos on table 2*/
 		}
 	} 
 
 	return FLAG;
 }
 
-void initTable (int table[]) {
+void initTable(int table[]) {
+
 	int i;
 
 	for (i = 0; i < M; i++) {
-		table[i] = FLAG;
+		table[i] = OPEN;
 	}
+}
+
+int hash_1(int key) {
+	return (key % M);
+}
+
+int hash_2(int key) {
+	return floor(M * (key * 0.9 - floor(key * 0.9)));
 }
--- myht.h
+++ myht.h
@@ -1,10 +1,36 @@
 #define M 11
 #define FLAG -5
+#define OPEN -4
 
+/*
+ * Funcion to insert values into tables
+ * Receives an int key and the two tables
+ */
 int insert(int key, int table_1[], int table_2[]);
+
 
 void printTable(int table[]);
 
+/*
+ * Function to search for values in the hash tables
+ * Receives an int key and the two tables
+ */
 int search(int key, int table_1[], int table_2[]);
 
+/*
+ * Function to initiate a hash table
+ * Receives the hash table to be initiated
+ */
 void initTable (int table[]);
+
+/*
+ * First hashing function
+ * Receives an int key to be hashed
+ */
+int hash_1(int key);
+
+/*
+ * Second hashing function
+ * Receives an int key to be hashed
+ */
+int hash_2(int key);
